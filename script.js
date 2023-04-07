9e5836994b562838e5fd33b3ea323bf967e0a45d
aravinthan98 <117471432+aravinthan98@users.noreply.github.com>
2023-04-07 12:30:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log("Hello World");
+// let name='aravinth'
+// let age=25
+// let gender='M'
+// console.log(name,age,gender);
+
+// let Car={
+//     wheel:5,
+//     powerstr:true,
+//     color:"white",
+//     "car vertion":"v10"
+
+// }
+// let arr=[1,2,3,4,5,6,7]
+
+// console.log(arr[0])
+// arr.push(9);
+
+// console.log(Car["car vertion"])
+// console.log(arr)
+// arr.pop();
+// console.log(arr)
+// arr.shift();
+// console.log(arr)
+
+// delete Car.wheel
+// console.log(Car)
+// Car=null;
+
+console.log(a);
+var a=15;
